108a79006d8ae8f035305d2b33f27ab826a25cd1
Th3LilNum05 <NSHARMA01@RIO.EDU>
2025-10-26 20:17:41 -0400

    Implement Contact, Node, and HashTable classes
    Implemented Contact, Node, and HashTable classes with methods for insertion, searching, and printing contacts.

--- hash_table.py
+++ hash_table.py
@@ -5,8 +5,11 @@ class Contact:
         name (str): The name of the contact.
         number (str): The phone number of the contact.
     '''
-    
-    pass # Delete this line when implementing the class
+    def init(self, name, number):
+        self.name = name
+        self.number = number
+    def str(self):
+        return f"{self.name}: {self.number}"
 
 class Node:
     '''
@@ -16,8 +19,10 @@ class Node:
         value (Contact): The value (Contact object) associated with the key.
         next (Node): Pointer to the next node in case of a collision.
     '''
-   
-    pass # Delete this line when implementing the class
+    def init(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
 class HashTable:
     '''
@@ -31,7 +36,43 @@ class HashTable:
         search(key): Searches for a contact by name.
         print_table(): Prints the structure of the hash table.
     '''
-    
-    pass # Delete this line when implementing the class
-
-# Test your hash table implementation here.  
+    def init(self, size):
+        self.size = size
+        self.data = [None] * size
+    def hash_function(self, key):
+        return sum(ord(c) for c in key) % self.size
+    def insert(self, key, number):
+        index = self.hash_function(key)
+        new_contact = Contact(key, number)
+        new_node = Node(key, new_contact)
+        if self.data[index] is None:
+            self.data[index] = new_node
+        else:
+            current = self.data[index]
+            while True:
+                if current.key == key:
+                    current.value.number = number
+                    return
+                if current.next is None:
+                    break
+                current = current.next
+            current.next = new_node
+    def search(self, key):
+        index = self.hash_function(key)
+        current = self.data[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
+    def print_table(self):
+        for i in range(self.size):
+            current = self.data[i]
+            if not current:
+                print(f"Index {i}: Empty")
+            else:
+                print(f"Index {i}:", end=" ")
+                while current:
+                    print(f"- {current.value}", end=" ")
+                    current = current.next
+                print()
